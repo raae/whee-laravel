3c084bee1d475f9f13b2a9e72bb71dd2a098025e
olavea <ola@olavea.com>
2025-08-05 17:57:44 +0200

    notes

---
+++ resources/views/booking.blade.php
@@ -0,0 +1,8 @@
+Et dashboard 
+div med text inn 
+"neste faste service" osv fra airtable 
+hente normalizer_get_raw_decomposition
+ny func i airtable service 
+ny controller som istedenfor å hente fra databasen, så henter den 
+
+det existing dashboard henter masse fra auth user
