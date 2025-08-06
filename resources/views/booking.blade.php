a64cb40fb1b6274cd8bf04cd8a8ca871ba0cbb19
olavea <ola@olavea.com>
2025-08-06 08:10:19 +0200

    created a dynamic route and moved the booking route to the bottom of web.php and display the dummy data in '/booking/{id}'

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -1,4 +1,25 @@
-<!-- Et dashboard 
+<div>
+    <!-- Links -->
+    <p><- Tilbake til whee.no  </p>
+    <p>Whee! - Min Whee! - Sykkelhjelp - Sykkel og abo - Kundeinfo - Bet - Reg og beti - </p>
+</div>
+<div>
+    <p>Neste faste service</p>
+    <h1>Whee! Sommerservice ☀️</h1>
+    <p>Time: {{ $booking['time'] }} på {{ $booking['location'] }}</p>
+</div>
+
+
+<div>
+    <!-- Buttons -->
+    <p>Endre  Kanseller  </p>
+</div>
+
+
+<!-- More stufff -->
+<!-- 
+Notes 05. Aug:
+Et dashboard 
 div med text inn 
 "neste faste service" osv fra airtable 
 hente 
@@ -6,7 +27,3 @@ ny func i airtable service
 ny controller som istedenfor å hente fra databasen, så henter den 
 
 det existing dashboard henter masse fra auth user -->
-
-<div>
-    <p>neste faste service</p>
-</div>
