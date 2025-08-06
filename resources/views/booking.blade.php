0d9299b960f043987e5b9dc7558576de1a441633
olavea <ola@olavea.com>
2025-08-06 07:31:43 +0200

    Created a new view 'booking' and a new route '/booking'  #2

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -1,8 +1,12 @@
-Et dashboard 
+<!-- Et dashboard 
 div med text inn 
 "neste faste service" osv fra airtable 
-hente normalizer_get_raw_decomposition
+hente 
 ny func i airtable service 
 ny controller som istedenfor å hente fra databasen, så henter den 
 
-det existing dashboard henter masse fra auth user
+det existing dashboard henter masse fra auth user -->
+
+<div>
+    <p>neste faste service</p>
+</div>
