aefd7052141719644fba908bab60a653eb226e76
olavea <ola@olavea.com>
2025-08-07 12:59:21 +0200

    Step 6 starting on the api call with a simple api about conferences that has 'time' and 'location' in the data #2

---
+++ app/Services/TestingCallPapersAPI.php
@@ -0,0 +1,21 @@
+<?php
+
+// Creating a PHP wrapper class around the API calls simplifies interaction and testing.
+// CallingAllPapers service class:
+// link to video https://laracasts.com/series/lets-build-a-saas-in-laravel/episodes/10
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class CallingAllPapers
+{
+    protected string $baseUrl = 'https://api.callingallpapers.com/v1/cfp';
+
+    public function getCFPs(): array
+    {
+        return Http::get($this->baseUrl)->json();
+    }
+
+    
+}
+
