7c76c5b85058cbd29f2c4b3667b64027dcfe6fa2
olavea <ola@olavea.com>
2025-08-10 09:36:24 +0200

    Can I use CallingAllPapers in routes > web.php? #2

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
+// Can I use CallingAllPapers in here?
+use App\Services\CallingAllPapers;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-
-
 
 // Authentication views
 Route::get('/auth/logg-inn', function () {
